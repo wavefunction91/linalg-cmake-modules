fc6b6e1c363c9087db072132e5fd973f5fd16adf
David Williams-Young <dbwy@lbl.gov>
2021-02-06 12:04:58 -0800

    [CI] Improve MKL correctness checking + Add check for correct ScaLAPACK linkage

--- .github/workflows/scripts/blas_libname.sh
+++ .github/workflows/scripts/blas_libname.sh
@@ -6,16 +6,18 @@ export int_type=$3
 
 if [ "$dist_name" == "intel-mkl" ]
 then
+  export mkl_base_name="mkl_intel_$3"
   if [ "$thread_name" == "sequential" ]
   then
-    echo "mkl_intel_$3.*mkl_sequential.*"
+    export mkl_thread_name="mkl_sequential"
   elif [ "$thread_name" == "openmp" ] 
   then
-    echo "mkl_intel_$3.*mkl_(gnu|intel|pgi)_thread.*"
+    export mkl_thread_name="mkl_(gnu|intel|pgi)_thread"
   elif [ "$thread_name" == "tbb" ] 
   then
-    echo "mkl_intel_$3.*mkl_tbb_thread.*"
+    export mkl_thread_name="mkl_tbb_thread"
   fi
+  echo "$mkl_base_name.*$mkl_thread_name"
 elif [ "$dist_name" == "netlib" ]
 then
   echo "blas"
