e392519686ef7cbf0bff0d0e0a967d799ba7edc9
David Williams-Young <dbwy@lbl.gov>
2021-02-08 19:39:42 +0000

    [MACROS] Cleanup

--- util/BLASUtilities.cmake
+++ util/BLASUtilities.cmake
@@ -39,32 +39,6 @@ foreach( _uplo LOWER UPPER )
 
     else()
 
-	    #if( _compile_output MATCHES "fortran" )
-      	    #  # Check for Standard Fortran Libraries
-      	    #  if(NOT STANDARDFORTRAN_LIBRARIES)
-      	    #    include(CMakeFindDependencyMacro)
-      	    #    find_dependency( StandardFortran )
-      	    #  endif()
-      	    #  list( APPEND ${_libs} ${STANDARDFORTRAN_LIBRARIES} )
-      	    #  set( ${_libs} ${${_libs}} PARENT_SCOPE )
-      	    #endif()
-
-      	    #if( _compile_output MATCHES "omp" )
-      	    #  if( NOT TARGET OpenMP::OpenMP_C )
-      	    #    find_dependency( OpenMP )
-      	    #  endif()
-      	    #  list( APPEND ${_libs} OpenMP::OpenMP_C )
-      	    #  set( ${_libs} ${${_libs}} PARENT_SCOPE )
-      	    #endif()
-
-      	    #if( _compile_output MATCHES "pthread" )
-      	    #  if( NOT TARGET Threads::Threads )
-      	    #    find_dependency( Threads )
-      	    #  endif()
-      	    #  list( APPEND ${_libs} Threads::Threads )
-      	    #  set( ${_libs} ${${_libs}} PARENT_SCOPE )
-      	    #endif()
-
       append_possibly_missing_libs( BLAS _compile_output ${_libs} _new_libs )
       list( APPEND ${_libs} ${_new_libs} )
       set( ${_libs} ${${_libs}} PARENT_SCOPE )
